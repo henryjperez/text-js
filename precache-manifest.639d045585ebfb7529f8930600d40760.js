5fd892a4528306db9a46f15a133432b7a357bed2
Quentin <quentinmiaurantinoillustrator@gmail.com>
2020-06-22 00:23:04 -0400

    Updates

---
+++ precache-manifest.639d045585ebfb7529f8930600d40760.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aae58461225555dab41011e6edde4a8",
+    "url": "/MiauMiauRobotics/Text.js/client/index.html"
+  },
+  {
+    "revision": "c071d5d9321a5f2b2c79",
+    "url": "/MiauMiauRobotics/Text.js/client/static/css/main.ebfb729c.chunk.css"
+  },
+  {
+    "revision": "c51e67ceb41ba617df1e",
+    "url": "/MiauMiauRobotics/Text.js/client/static/js/2.3c27b035.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/MiauMiauRobotics/Text.js/client/static/js/2.3c27b035.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c071d5d9321a5f2b2c79",
+    "url": "/MiauMiauRobotics/Text.js/client/static/js/main.0e1c98f9.chunk.js"
+  },
+  {
+    "revision": "61fcb9e3ce4f0aed53ce",
+    "url": "/MiauMiauRobotics/Text.js/client/static/js/runtime-main.99f9e624.js"
+  },
+  {
+    "revision": "cc04f08c5da3b58d59173708ef058481",
+    "url": "/MiauMiauRobotics/Text.js/client/static/media/dark-mode-false.cc04f08c.png"
+  },
+  {
+    "revision": "6b47009211249ffbbd9b70c7305c195c",
+    "url": "/MiauMiauRobotics/Text.js/client/static/media/dark-mode-true.6b470092.png"
+  },
+  {
+    "revision": "543a95deca291f2b1803190cb3b6f0e3",
+    "url": "/MiauMiauRobotics/Text.js/client/static/media/landing-image.543a95de.png"
+  },
+  {
+    "revision": "0bb90cf509e9b702fc4699f3d3c1130c",
+    "url": "/MiauMiauRobotics/Text.js/client/static/media/textjs-logo-dark.0bb90cf5.svg"
+  },
+  {
+    "revision": "c7dfb3884268b88def3ef079da992c9e",
+    "url": "/MiauMiauRobotics/Text.js/client/static/media/textjs-logo-light.c7dfb388.svg"
+  }
+]);
